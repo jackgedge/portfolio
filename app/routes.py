6894ca1cc6e98a5726fe71730c31601ea49c316e
Jack Galbraith-Edge <jack@jmge.io>
2026-09-06 21:54:25 +0100

    functioning concept

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,35 @@
-from flask import Blueprint, render_template
-from .webdav import folders, get_folder_path, get_folder_images
+from flask import Blueprint, render_template, send_file, send_from_directory
+from .webdav import get_folders, get_folder_images, get_image
 
 main_bp = Blueprint("main", __name__)
 
+@main_bp.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        "static",
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon"
+    )
+
 @main_bp.route('/', methods=['GET'])
 def index():
+    folders = get_folders()
     return render_template('index.html',
     folders=folders)
 
 @main_bp.route('/<folder>', methods=['GET', 'POST'])
 def folder(folder):
     images = get_folder_images(folder)
-    return render_template('folder.html',
-    images=images)
+
+    return render_template(
+        'folder.html',
+        images=images,
+        folder=folder)
+
+@main_bp.route('/<folder>/<image>')
+def image(folder, image):
+    local_path = get_image(folder, image)
+    return send_file(
+        local_path,
+        mimetype="image/jpeg"
+    )
--- app/webdav.py
+++ app/webdav.py
@@ -1,6 +1,7 @@
 from flask.cli import load_dotenv
 from webdav3.client import Client
 import os 
+import tempfile
 
 load_dotenv()
 
@@ -18,9 +19,30 @@ client = Client(options)
 
 PORTFOLIO_DIR: str | None = os.getenv('PORTFOLIO_DIR')
 
-folders = client.list(PORTFOLIO_DIR)[1:]
+def get_folders():
+    folders = client.list(PORTFOLIO_DIR)[1:]
+    folders_clean = []
+    for folder_name in folders:
+        folders_clean.append(folder_name.strip('/'))
+    return folders_clean
 
 def get_folder_images(folder):
     folder_path = f"{PORTFOLIO_DIR}/{folder}"
     images = client.list(folder_path)
     return images
+
+def get_image(folder, image):
+    remote_path = f"{PORTFOLIO_DIR}/{folder}/{image}".replace('//', '/')
+    extension = os.path.splitext(image)[1]
+    temp_file = tempfile.NamedTemporaryFile(
+        suffix=extension,
+        delete=False
+    )
+    temp_file.close()
+
+    client.download_sync(
+        remote_path=remote_path,
+        local_path=temp_file.name
+    )
+
+    return temp_file.name
